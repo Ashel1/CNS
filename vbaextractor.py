426c47be07c072ab36b604851409e3f6b5b4ffd6
Ashutosh Yadav <48183518+Ashel1@users.noreply.github.com>
2021-12-06 08:17:02 -0500

    Add files via upload

--- vbaextractor.py
+++ vbaextractor.py
@@ -1,5 +1,6 @@
 from win32com.client import Dispatch
 #Code used to extract the VBA code from an Excel file, if the Macro is an AutoRun this code might run the function, if not then it successfully extracts the code for analysis. 
+#All the VBA codes are designed to delete a folder (testfolder) in C:\, the delete file VBA deletes the test.txt file that must be created within the testfolder in teh C drive 
 wbpath = 'C:\\test.xlsm'
 xl = Dispatch("Excel.Application")
 xl.Visible = 1
